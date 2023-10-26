28e97d7067228e8b504f718a060aad85c5316b57
Samuel Gómez <sagomadev@gmail.com>
2023-10-26 12:37:31 +0200

    Add physics to scene

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 import "./style.css";
 import Phaser from "phaser";
 
+const sizes = {
+  width: 500,
+  height: 500,
+};
+
+const speedDown = 300;
+
 const config = {
   type: Phaser.WEBGL,
-  with: 500,
-  height: 500,
+  with: sizes.width,
+  height: sizes.height,
   canvas: gameCanvas,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: speedDown },
+      debug: true,
+    },
+  },
 };
 
 const game = new Phaser.Game(config);
