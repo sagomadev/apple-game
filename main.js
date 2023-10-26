762569f25dd5f898c8e03c77872c118ed062790c
Samuel Gómez <sagomadev@gmail.com>
2023-10-26 10:58:21 +0200

    Add JS Canvas Config

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 import "./style.css";
+import Phaser from "phaser";
+
+const config = {
+  type: Phaser.WEBGL,
+  with: 500,
+  height: 500,
+  canvas: gameCanvas,
+};
+
+const game = new Phaser.Game(config);
